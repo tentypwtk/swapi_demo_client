a4faf830fbebe1937fdaff46536fe4f0d55e9779
Wojtek <wojciech.fijolek@gmail.com>
2020-04-04 15:30:04 +0200

    register gson factory + rx java factory in retrofit

--- app/src/main/java/com/grapplo/swapidemo/di/AppComponent.kt
+++ app/src/main/java/com/grapplo/swapidemo/di/AppComponent.kt
@@ -4,7 +4,8 @@ import com.grapplo.swapidemo.BuildConfig
 import com.grapplo.swapidemo.api.ApiClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 object AppComponent {
@@ -13,7 +14,8 @@ object AppComponent {
             Retrofit
                 .Builder()
                 .baseUrl(BuildConfig.API_BASE_URL)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
         factory<ApiClient> {
