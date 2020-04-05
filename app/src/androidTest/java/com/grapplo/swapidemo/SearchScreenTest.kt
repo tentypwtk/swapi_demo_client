9d52852ace288318f9bdaabf25b2fe41faee588e
Wojtek <wojciech.fijolek@gmail.com>
2020-04-06 00:12:34 +0200

    requirement - call other endpoint when dropdown changes

--- app/src/androidTest/java/com/grapplo/swapidemo/SearchScreenTest.kt
+++ app/src/androidTest/java/com/grapplo/swapidemo/SearchScreenTest.kt
@@ -13,18 +13,38 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.grapplo.swapidemo.api.ApiClient
+import com.grapplo.swapidemo.api.response.SwapiResponse
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import io.reactivex.Single
 import io.reactivex.plugins.RxJavaPlugins
 import io.reactivex.schedulers.TestScheduler
+import org.hamcrest.CoreMatchers.any
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.core.IsNot.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.Koin
+import org.koin.test.KoinTest
+import org.koin.test.mock.MockProviderRule
+import org.koin.test.mock.declareMock
+import org.mockito.Mockito
 import java.util.concurrent.TimeUnit
 
 @RunWith(AndroidJUnit4::class)
-class SearchScreenTest {
+class SearchScreenTest : KoinTest {
+
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        Mockito.mock(clazz.java)
+    }
+
+    lateinit var apiClient: ApiClient
 
     @Before
     fun setup() {
@@ -81,6 +101,32 @@ class SearchScreenTest {
         onView(allOf(isAssignableFrom(TextView::class.java), withText("Planet"))).check(matches(isDisplayed()))
         onView(allOf(isAssignableFrom(TextView::class.java), withText("Person"))).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun changingSpinnerChangesApiEndpoint() {
+        // given
+        apiClient = declareMock {
+            given(searchPlanet(any())).willReturn(Single.just(SwapiResponse(0, null, null, emptyList())))
+            given(searchPerson(any())).willReturn(Single.just(SwapiResponse(0, null, null, emptyList())))
+        }
+        launchActivity<MainActivity>()
+
+        // when
+        onView(withId(R.id.subject_picker)).perform(click())
+        onView(withText("Person")).perform(click())
+        onView(withId(R.id.search_input)).perform(typeText("A"))
+        testScheduler.advanceTimeBy(3, TimeUnit.SECONDS)
+        onView(withId(R.id.subject_picker)).perform(click())
+        onView(withText("Planet")).perform(click())
+        onView(withId(R.id.search_input)).perform(typeText("B"))
+        testScheduler.advanceTimeBy(3, TimeUnit.SECONDS)
+
+        // then
+        verify(apiClient, times(1)).searchPlanet(any())
+        verify(apiClient, times(1)).searchPerson(any())
+    }
+
+
 
 
 }
--- app/src/main/java/com/grapplo/swapidemo/api/ApiClient.kt
+++ app/src/main/java/com/grapplo/swapidemo/api/ApiClient.kt
@@ -1,6 +1,7 @@
 package com.grapplo.swapidemo.api
 
 import com.grapplo.swapidemo.api.response.SwapiResponse
+import com.grapplo.swapidemo.domain.Person
 import com.grapplo.swapidemo.domain.Planet
 import io.reactivex.Single
 import retrofit2.http.*
@@ -8,4 +9,7 @@ import retrofit2.http.*
 interface ApiClient {
     @GET("planets")
     fun searchPlanet(@Query("search") phrase: String): Single<SwapiResponse<Planet>>
+    @GET("people")
+    fun searchPerson(@Query("search") phrase: String): Single<SwapiResponse<Person>>
+
 }
