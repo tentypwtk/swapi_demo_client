02a9d5795582bcd95f1d2923c1472ab2d89d9c5f
Wojtek <wojciech.fijolek@gmail.com>
2020-04-07 22:53:07 +0200

    detail screen with using data passed from search

--- app/src/main/java/com/grapplo/swapidemo/presentation/DetailsFragment.kt
+++ app/src/main/java/com/grapplo/swapidemo/presentation/DetailsFragment.kt
@@ -1,5 +1,43 @@
 package com.grapplo.swapidemo.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.grapplo.swapidemo.databinding.DetailsLayoutBinding
+import com.grapplo.swapidemo.domain.SwEntity
 
-class DetailsFragment : Fragment()
+class DetailsFragment : Fragment() {
+
+    private lateinit var dataBinding: DetailsLayoutBinding
+
+    private val backHandler = object: OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = DetailsLayoutBinding.inflate(inflater, container, false).let {
+        this@DetailsFragment.dataBinding = it
+        it.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, backHandler)
+        arguments?.getParcelable<SwEntity>("@string/argument_details")
+            ?.let { it.name to it.description }
+            ?.let { (title, descr) ->
+                dataBinding.title = title
+                dataBinding.description = descr
+                dataBinding.invalidateAll()
+            }
+    }
+}
